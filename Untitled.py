ada97c7b05e2679a433cc4664847d9ad2cdc69aa
Izeckio <c3666@student.leedsbeckett.ac.uk>
2025-11-29 17:58:28 +0000

    Setting up a basic metric pull using Prometheus

--- Untitled.py
+++ Untitled.py
@@ -1,1 +1,7 @@
-print("Hello world")
+from prometheus_api_client import PrometheusConnect
+
+
+prom = PrometheusConnect(url="http://localhost:9090", disable_ssl=True) #Connecting to Promotheus
+
+cpu_usage = prom.get_current_metric_value(metric_name="container_cpu_usage_seconds_total")
+print(cpu_usage) #Basic implementation to show pulling metric from Prometheus
